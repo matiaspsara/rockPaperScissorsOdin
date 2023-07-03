3ae611831b116132cebea5f4f20ff52f06c9a168
Matias <matiaspsara@gmail.com>
2023-07-03 20:26:11 -0300

    Created function getComputerChoice

---
+++ script.js
@@ -0,0 +1,17 @@
+function getComputerChoice(){
+    let choice=Math.floor(Math.random() * 3) + 1
+    switch(choice){
+        case 1:
+            return "Rock";
+            break
+        case 2:
+            return "Paper";
+            break
+        case 3:
+            return "Scissors"
+            break
+        default:
+            return "Random is not working correctly"
+    }
+    
+}
