8d341669f48e5d52faf41445f5206c593ad68b7a
Matias <matiaspsara@gmail.com>
2023-07-03 21:10:13 -0300

    Finished the basic project

--- script.js
+++ script.js
@@ -13,5 +13,51 @@ function getComputerChoice(){
         default:
             return "Random is not working correctly"
     }
-    
 }
+function playRound(playerSelection,computerSelection){
+    pSelection=playerSelection.toUpperCase();
+    cSelection=computerSelection.toUpperCase();
+    switch(true){
+        case(pSelection=="ROCK" && cSelection=="PAPER"):
+            return "You Lose! Paper beats Rock";
+            break
+        case(pSelection=="ROCK" && cSelection=="SCISSORS"):
+            return "You Win! Rock beats Scissors";
+            break
+        case(pSelection=="ROCK" && cSelection=="ROCK"):
+            return "Its a tie";
+            break
+        case(pSelection=="PAPER" && cSelection=="ROCK"):
+            return "You Win! Paper beats Rock";
+            break
+        case(pSelection=="PAPER" && cSelection=="SCISSORS"):
+            return "You Lose! Scissors beats Paper";
+            break
+        case(pSelection=="PAPER" && cSelection=="PAPER"):
+            return "Its a tie";
+            break
+        case(pSelection=="SCISSORS" && cSelection=="ROCK"):
+            return "You Lose! Rock beats Scissors";
+            break
+        case(pSelection=="SCISSORS" && cSelection=="PAPER"):
+            return "You Win! Scissors beats Paper";
+            break
+        case(pSelection=="SCISSORS" && cSelection=="SCISSORS"):
+            return "Its a tie";
+            break
+        default:
+            return "You misspelled your input, try again!";
+            break  
+    }
+
+
+}
+
+function game(){
+    for(let i=0;i<5;i++){
+        let playerChoice=prompt("Enter Rock Paper or Scissors");
+        let computerSelection = getComputerChoice(); 
+        console.log(playRound(playerChoice, computerSelection));
+    }
+}
+game();
